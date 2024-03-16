9bbbea5942bf7bc83487a758eba4137c0dd97b5c
skelmis <ethanmkh2@gmail.com>
2024-03-16 16:54:05 +1300

    fix: xss haha

--- app.py
+++ app.py
@@ -8,13 +8,21 @@ from fastapi import FastAPI
 from piccolo_admin.endpoints import create_admin
 from piccolo.engine import engine_finder
 from starlette.requests import Request
-from starlette.responses import HTMLResponse
-from starlette.routing import Route, Mount
+from starlette.responses import HTMLResponse, Response
+from starlette.routing import Mount
 from starlette.staticfiles import StaticFiles
 
 from home.piccolo_app import APP_CONFIG
 from home.tables import RequestMade
 
+headers = {
+    "x-frame-options": "SAMEORIGIN",
+    "x-xss-protection": "1; mode=block",
+    "x-content-type-options": "nosniff",
+    "referrer-policy": "strict-origin",
+    "permissions-policy": "microphone=(); geolocation=(); fullscreen=();",
+    # "content-security-policy": "default-src 'none'; script-src 'none'",
+}
 
 app = FastAPI(
     routes=[
@@ -34,7 +42,8 @@ app = FastAPI(
 ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(
         searchpath=os.path.join(os.path.dirname(__file__), "home", "templates")
-    )
+    ),
+    autoescape=True,
 )
 
 
@@ -59,7 +68,7 @@ async def view_request(request_uuid: uuid.UUID):
     "/{full_path:path}",
     methods=["GET", "POST", "HEAD", "DELETE", "OPTIONS", "TRACE", "PUT", "PATCH"],
 )
-async def catch_all(request: Request, full_path: str):
+async def catch_all(request: Request, full_path: str, response: Response):
     template = ENVIRONMENT.get_template("home.html.jinja")
     request_made: RequestMade = RequestMade(
         headers=orjson.dumps(dict(request.headers)).decode("utf-8"),
@@ -73,8 +82,12 @@ async def catch_all(request: Request, full_path: str):
     requests = (
         await RequestMade.objects().order_by(RequestMade.id, ascending=False).limit(25)
     )
+
     content = template.render(title="Incoming requests", requests=requests)
-    return HTMLResponse(content)
+    return HTMLResponse(
+        content,
+        headers=headers,
+    )
 
 
 @app.on_event("startup")
